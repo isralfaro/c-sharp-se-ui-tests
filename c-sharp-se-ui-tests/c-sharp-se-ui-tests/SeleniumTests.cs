e0ca60aab8cf41c82c0d1456038a115eae957d5f
Israel Alfaro <isralfaro@gmail.com>
2024-04-01 20:16:32 -0600

    Initial project setup
    Setup C# project and dependencies

---
+++ c-sharp-se-ui-tests/c-sharp-se-ui-tests/SeleniumTests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace c_sharp_se_ui_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
